d1cf755395ca978a840540722def1297099353b9
Yoav Weller <yoav.weller@gmail.com>
2022-11-06 18:47:09 +0200

    Update RDT.py
    Fixing the random_timestamp_generator function (now it works).

--- Python/RDT.py
+++ Python/RDT.py
@@ -2,7 +2,6 @@
 import random
 import datetime
 
-
 def random_date_generator(start_year: int, start_month: int, start_day: int, end_year: int, end_month: int, end_day: int):
     
     start_date = datetime.date(start_year, start_month, start_day)
@@ -14,15 +13,22 @@ def random_date_generator(start_year: int, start_month: int, start_day: int, end
     random_date = start_date + datetime.timedelta(days=random_number_of_days)
 
     return random_date
-"""
+
 def random_timestamp_generator(opening_hour, opening_minute, opnening_second, closing_hour, closing_minute, closing_second):
+
+    random_hour = random.randint(opening_hour, closing_hour)
+    if random_hour == opening_hour:
+        random_minute = random.randint(opening_minute, 59)
+        random_second = random.randint(opnening_second, 59)
+    elif random_hour == closing_hour:
+        random_minute = random.randint(0, closing_minute)
+        random_second = random.randint(0, closing_second)
+    else:
+        random_minute = random.randint(0, 59)
+        random_second = random.randint(0, 59) 
     
-    opening_time = datetime.time(opening_hour, opening_minute, opnening_second)
-    closing_time = datetime.time(closing_hour, closing_minute, closing_second)
+    random_timestamp = datetime.time(random_hour, random_minute, random_second)
 
-    opreation_time = closing_time - opening_time
+    return random_timestamp
 
-    print(opreation_time)
-
-random_timestamp_generator(9, 0, 0, 21, 30, 0)
-"""
+print(random_timestamp_generator(9, 0, 0, 22, 30, 0))
