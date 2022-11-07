97dd61b4c0b13c6c0a4439ffc0bb187f4b0b1a3a
LolipopnJoker <yoav.weller@gmail.com>
2022-11-07 08:55:23 +0200

    Update RTSG_examples.py
    Fixing the import and adding example for the random_timestamp_generator function.

--- Python/RTSG_examples.py
+++ Python/RTSG_examples.py
@@ -1,6 +1,10 @@
 # Imports
-from RDT import random_date_generator
+from RTSG import *
 
 """ Example 1 - Generating a random date """
 rand_date = random_date_generator(2009, 10, 14, 2022, 10, 14)
 print(rand_date)
+
+""" Example 2 - Generating a random timestamp """
+rand_timestamp = random_timestamp_generator(9, 0, 0, 21, 30, 0)
+print(rand_timestamp)
